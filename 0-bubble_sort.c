03f8921914ba3d0942d92df4f0ccf2f7595b6922
johntymuriithi <muriithijohn634@gmail.com>
2023-10-21 04:39:23 -0700

    files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,11 +4,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	int swapped;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -16,7 +14,6 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
 				print_array(array, size);
 			}
 		}
