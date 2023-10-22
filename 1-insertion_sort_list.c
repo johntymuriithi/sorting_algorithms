4760e7ed4c4684af57172aa438ef3dd1337eca88
johntymuriithi <muriithijohn634@gmail.com>
2023-10-22 07:41:29 -0700

    this insertion is sth else

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,41 +8,41 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *sorted = NULL;
-    listint_t *current = *list;
-    listint_t *next;
-    listint_t *current_sorted;
+	listint_t *sorted = NULL;
+	listint_t *current = *list;
+	listint_t *next;
+	listint_t *current_sorted;
 
-    while (current != NULL)
-    {
-        next = current->next;
-        if (sorted == NULL || sorted->n >= current->n)
-        {
-            current->next = sorted;
-            if (sorted != NULL)
-            {
-                sorted->prev = current;
-            }
-            current->prev = NULL;
-            sorted = current;
-        }
-        else
-        {
-            current_sorted = sorted;
-            while (current_sorted->next != NULL && current_sorted->next->n < current->n)
-            {
-                current_sorted = current_sorted->next;
-            }
-            current->next = current_sorted->next;
-            if (current_sorted->next != NULL)
-            {
-                current_sorted->next->prev = current;
-            }
-            current->prev = current_sorted;
-            current_sorted->next = current;
-        }
-        current = next;
-    	*list = sorted;
-	print_list(*list);
-    }
+	while (current != NULL)
+	{
+		next = current->next;
+		if (sorted == NULL || sorted->n >= current->n)
+		{
+			current->next = sorted;
+			if (sorted != NULL)
+			{
+				sorted->prev = current;
+			}
+			current->prev = NULL;
+			sorted = current;
+		}
+		else
+		{
+			current_sorted = sorted;
+			while (current_sorted->next != NULL && current_sorted->next->n < current->n)
+			{
+				current_sorted = current_sorted->next;
+			}
+			current->next = current_sorted->next;
+			if (current_sorted->next != NULL)
+			{
+				current_sorted->next->prev = current;
+			}
+			current->prev = current_sorted;
+			current_sorted->next = current;
+		}
+		current = next;
+		*list = sorted;
+		print_list(*list);
+	}
 }
